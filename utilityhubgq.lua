aac813a50a2607a47fdf482d6b1c3e6a951af1aa
gqsource <136137182+gqsource@users.noreply.github.com>
2023-06-13 21:59:09 -0400

    Update utilityhubgq.lua

--- utilityhubgq.lua
+++ utilityhubgq.lua
@@ -165,4 +165,108 @@ Section2:AddButton({
         Time = 5
     })
 OrionLib:Init()   
+
+
+
+
+
+elseif game.PlaceId == 13284033561 then
+
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "UtilityHub | Fishing Frenzy Simulator", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+    local Home = Window:MakeTab({
+        Name = "Home",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+
+    local Section1 = Window:MakeTab({
+        Name = "Auto Farm",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+    
+    Home:AddParagraph("UtilityHub -N- PLAY-","Sand Dog#5240 / ! Sunn#5342")
+
+    Home:AddButton({
+        Name = "UtilityHub Discord Server Copy to Clipboard:",
+        Callback = function()
+            setclipboard("discord.gg/wFYeV7wnD3")
+          end    
+    })
+
+    Home:AddButton({
+        Name = "Destroy Gui",
+        Callback = function()
+            OrionLib:Destroy()
+          end    
+    })
+
+    OrionLib:MakeNotification({
+        Name = "UtilityHub N- PLAY-",
+        Content = "Credits: Sand Dog / ! Sunn",
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+    
+    
+
+    Section1:AddDropdown({
+        Name = "Selete Zone To Farm",
+        Default = "1",
+        Options = {"1", "2"},
+        Callback = function(Value)
+            print(Value)
+        end    
+    })
+    
+
+    Section1:AddToggle({
+        Name = "Auto Fish!",
+        Default = false,
+        Callback = function(v)
+            _G.AutoFish = v
+
+            while _G.AutoFish == true do
+
+                local Remote = Instance.new("RemoteEvent").FireServer;
+
+                local A_1 = "Reel"
+                Remote(game:GetService("ReplicatedStorage").Remotes.Rod, A_1)
+                task.wait()
+
+                
+                local Remote2 = Instance.new("RemoteEvent").FireServer;
+
+                local ohString1 = "Cast"
+                local ohVector32 = Vector3.new(-105.57073974609375, 8.82790756225586, 325.18109130859375)
+                local ohString3 = "Grassy"
+                
+                Remote2(game:GetService("ReplicatedStorage").Remotes.Rod, ohString1, ohVector32, ohString3)
+                task.wait()
+            end
+        end    
+    })
+
+    Section1:AddToggle({
+        Name = "Auto Train!",
+        Default = false,
+        Callback = function(v)
+            _G.AutoTrain = v
+
+            while _G.AutoTrain == true do
+
+                local Remote = Instance.new("RemoteEvent").FireServer;
+
+                local A_1 = "Train"
+                local A_2 = game:GetService("Workspace").Training.Grassy.Hole
+                Remote(game:GetService("ReplicatedStorage").Remotes.Train, A_1, A_2)
+                Remote(game:GetService("ReplicatedStorage").Remotes.Train, A_1, A_2)
+                Remote(game:GetService("ReplicatedStorage").Remotes.Train, A_1, A_2)
+                Remote(game:GetService("ReplicatedStorage").Remotes.Train, A_1, A_2)
+                task.wait()
+        end 
+    end
+    })
 end
